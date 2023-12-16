888954057b33eee23b20eb2ad647a9ad3d1e9fd0
Olga Chernega <eliang.cheng@gmail.com>
2023-12-16 12:43:51 +0200

    feat: add basicAuthorizer handler lambda

--- authorization-service/src/handlers/basicAuthorizer.ts
+++ authorization-service/src/handlers/basicAuthorizer.ts
@@ -1,1 +1,37 @@
-export const handler = async () => {};
+import {
+  APIGatewayAuthorizerResult,
+  APIGatewayRequestAuthorizerEvent,
+} from 'aws-lambda';
+import { generatePolicy } from '../utils/helpers';
+
+export const handler = async (
+  event: APIGatewayRequestAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+  try {
+    console.log('BasicAuthorizer:', JSON.stringify(event));
+
+    const authorizationToken = event.headers?.authorization;
+    if (!authorizationToken) {
+      throw new Error('Unauthorized');
+    }
+
+    const [authType, encodedToken] = authorizationToken.split(' ');
+    if (authType !== 'Basic' || !encodedToken) {
+      return generatePolicy(authorizationToken, 'Deny', event.methodArn);
+    }
+
+    const [username, password] = Buffer.from(encodedToken, 'base64')
+      .toString('utf8')
+      .split(':');
+    const storedPassword = process.env[username.toLowerCase()];
+    const effect =
+      storedPassword && storedPassword === password ? 'Allow' : 'Deny';
+
+    const policy = generatePolicy(authorizationToken, effect, event.methodArn);
+    return policy;
+  } catch (err: unknown) {
+    const error = err as Error;
+    console.error('An error occurred:', error.message);
+    throw new Error('Unauthorized');
+  }
+};
