da21c7147db7e01e4ba2ab004ba86ce00dd2ff2f
Olga Chernega <eliang.cheng@gmail.com>
2024-01-04 17:15:14 +0200

    feat: add bff-service and caching

---
+++ bff-service/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { CACHE_TTL } from './constants';
+
+@Module({
+  imports: [CacheModule.register({ ttl: CACHE_TTL })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
